7c78ba0d41ff8be4873841da8931dec2f42262a9
majekkabiola <majekabiola1@gmail.com>
2022-10-14 02:52:15 -0700

    c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,14 @@
 #include <string.h>
 
 /**
- *
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 
-int main(void)
+int main (void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("/"Programming is like building a multilingual puzzle");
 
 return (0);
 }
-
