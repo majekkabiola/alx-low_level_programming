e597ea2a23800e74dd79962ba65060d05e94832a
majekkabiola <majekabiola1@gmail.com>
2022-10-18 04:13:28 -0700

    char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/** main - prints main follwed by a new line
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+_putchar('A');
+_putchar('G');
+_putchar('A');
+_putchar('P');
+_putchar('I');
+_putchar('\n');
+return (0);
+
+}
+
