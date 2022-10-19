32abe21b2fdd5f1c535ad6b7f6290bcc3b504a47
majekkabiola <majekabiola1@gmail.com>
2022-10-19 09:24:56 -0700

    code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
 
 {
 	_putchar('_');
@@ -20,6 +20,6 @@ int main(void);
 	_putchar('r');
 	_putchar('\n');
 
-	Return(0);
+	return (0);
 }
 
