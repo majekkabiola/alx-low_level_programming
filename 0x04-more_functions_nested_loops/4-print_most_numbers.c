b2a01731d11208aeff8e7c67f485ccf936e6e58a
majekkabiola <majekabiola1@gmail.com>
2022-10-20 08:04:47 -0700

    code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_most_numbers - prints 01356789\n
  *
@@ -9,12 +10,12 @@
 void print_most_numbers(void)
 
 {
-	char c;
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
-	}
-	_putchar('\n');
-}
 
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
+}
+_putchar('\n');
+}
