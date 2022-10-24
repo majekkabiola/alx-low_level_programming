f6940d4a238239bbb6d1d5272c5a92b16c85bf58
majekkabiola <majekabiola1@gmail.com>
2022-10-24 08:20:11 -0700

    pointers_array

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of the integers
+ * @a: int a
+ * @b: int b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
+
