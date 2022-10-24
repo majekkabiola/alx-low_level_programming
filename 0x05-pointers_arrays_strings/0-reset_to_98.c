7c186501061f95ea6ec9d719c1721f41a0a71a0a
majekkabiola <majekabiola1@gmail.com>
2022-10-24 07:52:59 -0700

    poinnters and array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - update the value of n to 98
+ * reset_to_98 - update the value of n to 98
  * @n: input
  *
  * Return: n
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
+
 }
